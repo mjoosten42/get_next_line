18b8fa86fe9d69f21390db60c9a476e23662fdd4
Maarten Joosten <mjoosten@f1r4s4.codam.nl>
2021-12-16 10:11:10 +0100

    defined BUFFER_SIZE

--- get_next_line.h
+++ get_next_line.h
@@ -6,32 +6,20 @@
 /*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 14:50:38 by mjoosten          #+#    #+#             */
-/*   Updated: 2021/11/12 16:29:24 by mjoosten         ###   ########.fr       */
+/*   Updated: 2021/12/16 10:10:45 by mjoosten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
-# include <unistd.h>
-
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
-typedef struct s_list
-{
-	char			*content;
-	struct s_list	*next;
-}					t_list;
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
-
-t_list	*ft_lstnew(char *content);
-t_list	*ft_lstadd_back(t_list **lst, t_list *new);
-char	*ft_lstclear(t_list **lst);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *s);
 
 #endif
